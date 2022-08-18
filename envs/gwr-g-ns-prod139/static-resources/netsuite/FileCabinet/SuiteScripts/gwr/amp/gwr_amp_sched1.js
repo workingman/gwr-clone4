a5b11d31a2ca6e5c417f9faabce47cdc0468be3c
Geoffrey Routledge <geoff@dgbi.ca>
2022-08-18 19:31:54 +0000

    baseline

---
+++ envs/gwr-g-ns-prod139/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/amp/gwr_amp_sched1.js
@@ -0,0 +1,23 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ScheduledScript
+ */
+define(['N/log'],
+    /**
+ * @param{log} log
+ */
+    (log) => {
+
+        /**
+         * Defines the Scheduled script trigger point.
+         * @param {Object} scriptContext
+         * @param {string} scriptContext.type - Script execution context. Use values from the scriptContext.InvocationType enum.
+         * @since 2015.2
+         */
+        const execute = (scriptContext) => {
+
+        }
+        // custbody_vat_id customworkflowworkflow5_v2_2_3.custworkflow13_2
+        return {execute}
+
+    });
