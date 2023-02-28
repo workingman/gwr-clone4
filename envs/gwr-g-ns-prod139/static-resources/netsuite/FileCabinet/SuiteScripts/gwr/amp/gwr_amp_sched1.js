b9fd8a0e2af7c14b1550141439cd9c1c1e511ead
Geoffrey Routledge <geoff@dgbi.ca>
2023-02-28 18:28:55 +0000

    BIZAPP-421 baseline SO work

--- envs/gwr-g-ns-prod139/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/amp/gwr_amp_sched1.js
+++ envs/gwr-g-ns-prod139/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/amp/gwr_amp_sched1.js
@@ -17,7 +17,6 @@ define(['N/log'],
         const execute = (scriptContext) => {
             var otherFieldList = ['custbody_jg_quote_number','custbody32','custbody39','otherrefnum','leadsource','partner','department','intercotransaction'];
         }
-        // custbody_vat_id customworkflowworkflow5_v2_2_3.custworkflow13_2
         return {execute}
 
     });
